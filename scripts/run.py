6cd2790619b15663a0a2066efdbafb06f61d373e
rob-andrew-ellis <robandrewellis@gmail.com>
2024-10-26 00:52:02 +0100

    Added function to add items to the list

--- scripts/run.py
+++ scripts/run.py
@@ -1,4 +1,9 @@
-from shoppy.shopping_list import get_list
+from __future__ import annotations
+
+from shoppy.shopping_list import add_item, get_list
 
 if __name__ == "__main__":
-    print(get_list())
+    my_list = get_list()
+
+    print(my_list)
+    print(add_item(my_list, input("\nAdd an item to the list: ")))
--- src/shoppy/shopping_list.py
+++ src/shoppy/shopping_list.py
@@ -1,9 +1,38 @@
+from __future__ import annotations
+
 from pathlib import Path
 
 
 def get_list(list_path: Path = Path("data/list.txt")) -> list[str]:
+    """If the shopping list is saved, it is loaded, otherwise an empty list is
+       returned
+
+    Args:
+        list_path: Path to saved list file
+
+    Returns:
+        A list of Strings representing the shopping list
+    """
     if list_path.is_file():
         with open(list_path) as f:
             return [line.rstrip("\n") for line in f]
     else:
         return []
+
+
+def add_item(my_list: list[str], item: str) -> list[str]:
+    """Adds a new item to the list, only if the item is not already present
+
+    Args:
+        my_list: The shopping list
+        item: The name of the item to be added
+
+    Returns:
+        The shopping list with the new item appended
+    """
+    if item not in my_list:
+        my_list.append(item)
+    else:
+        print(f"{item} is already on the list, silly.")
+
+    return my_list
